ae64245e869ba96badd8cf0fc64f6c567e460dd5
Bo He <qq111372757@gmail.com>
2026-02-15 15:17:01 +0800

    migrate dependence liquid-dsp to branch master from develop

--- include/quiet/common.h
+++ include/quiet/common.h
@@ -10,6 +10,29 @@
 
 #include <liquid/liquid.h>
 
+/* Shim for resamp_rrrf_execute_output_block, which only exists in
+   liquid-dsp's devel branch.  We emulate it by calling the per-sample
+   resamp_rrrf_execute in a loop so we can stop as soon as the output
+   buffer is full. */
+static inline void resamp_rrrf_execute_output_block(
+    resamp_rrrf _q,
+    const float *_x, unsigned int _nx,  unsigned int *_nread,
+    float *_y,  unsigned int _ny_max,  unsigned int *_nwritten)
+{
+    unsigned int rd = 0, wr = 0;
+    float rate = resamp_rrrf_get_rate(_q);
+    unsigned int worst = (unsigned int)ceilf(rate) + 1;
+
+    while (rd < _nx && wr + worst <= _ny_max) {
+        unsigned int nw;
+        resamp_rrrf_execute(_q, _x[rd], _y + wr, &nw);
+        rd++;
+        wr += nw;
+    }
+    *_nread    = rd;
+    *_nwritten = wr;
+}
+
 typedef quiet_sample_t sample_t;
 typedef quiet_dc_filter_options dc_filter_options;
 typedef quiet_resampler_options resampler_options;
